1295683761df0af1734f2df0ae8abf99ca950018
Gary <zipe.daden@gmail.com>
2024-06-30 22:05:32 +0800

    移除檔案

--- webservice-server/build/resources/main/sql/SQL_SELECT_RESPONSE_CONTENT.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    mr.RESPONSE_CONTENT AS content
-FROM
-	MOCK_RESPONSE mr
-WHERE
-    mr.PUBLISH_URL = :publishUrl
-  AND mr.METHOD = :method
-  AND mr.CONDITION = :condition
-  AND mr.IS_ACTIVE = TRUE
